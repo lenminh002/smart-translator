884b727745d644904aaa0a20801705bffa463d88
Le Nguyen Minh <lenminh002@gmail.com>
2026-02-19 12:16:52 -0800

    adjust index.html and add demo translation system

---
+++ app/ai.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+
+from google import genai
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=gemini_api_key)
+
+systemInstruction = """
+            You are a helpful assistant that translates text from one language to another.
+            Only translate the text and DO NOT provide any additional information or explanations.
+            Make sure to maintain the original meaning and context of the text while translating.
+                    """
+
+async def translate_text(text, target_language):
+    response = await client.aio.models.generate_content(
+        model="gemini-3-flash-preview",
+        config={
+            "system_instruction": systemInstruction,
+            "temperature": 0.3
+        },
+        contents=[
+            {
+                "role": "user",
+                "parts": [{"text": f"Translate the following text to {target_language}: {text}"}]
+            }
+        ]
+    )
+    return response.text
+
+
+
+# for testing purposes:
+if __name__ == "__main__":
+    import asyncio
+
+    result = asyncio.run(translate_text("Hello, how are you?", "Vietnamese"))
+    print(result)
--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 #run command: python3 app/main.py
 
+from flask import Flask, render_template, request, jsonify
+from ai import translate_text
 
-from flask import Flask, render_template
-from dotenv import load_dotenv
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+# @app.route("/translate", methods=["POST"])
+# def translate():
+#     text = request.form["text"]
+#     translated_text = translate_text(text)
+#     return render_template("index.html", translated_text=translated_text)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
