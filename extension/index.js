e54e59a9aa7b209682613d733c18602bc0278d4c
Le Nguyen Minh <lenminh002@gmail.com>
2026-02-20 14:38:40 -0800

    able to work on chrome extension

---
+++ extension/index.js
@@ -0,0 +1,51 @@
+const inputElement = document.getElementById("input-text");
+const langElement = document.getElementById("target-language");
+const outputElement = document.getElementById("output-text");
+const btnElement = document.getElementById("translate-btn");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    btnElement.addEventListener('click', translateText);
+});
+
+
+async function translateText() {
+    const text = inputElement.value.trim();
+    const targetLanguage = langElement.value.trim();
+
+    if (text === "" || targetLanguage === "") {
+        alert("Please enter text and select a target language.");
+        return;
+    }
+    else {
+        try {
+
+            btnElement.disabled = true;
+            btnElement.textContent = "Translating...";
+
+
+            const res = await fetch("http://127.0.0.1:8000/translate", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ text, targetLanguage })
+            });
+
+            const data = await res.json();
+            if (!res.ok) {
+                throw new Error(data.error || "Translation failed");
+            }
+
+            outputElement.textContent = data.reply;
+        }
+        catch (error) {
+            alert("Error: " + error.message);
+            return;
+        }
+        finally {
+            btnElement.disabled = false;
+            btnElement.textContent = "Translate";
+        }
+
+
+    }
+}
